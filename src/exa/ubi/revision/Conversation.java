64b96e98f41362cbd378cb1fb0eca17d6f08e227
KarlaMtz <karlamartinest@hotmail.com>
2017-11-22 00:03:43 -0600

    Nuevo commit

--- src/exa/ubi/revision/Conversation.java
+++ src/exa/ubi/revision/Conversation.java
@@ -2,6 +2,6 @@ package exa.ubi.revision;
 
 public class Conversation {
 	public void revisar_conv(){
-		System.out.println("Este metodo CONVERSATION aun no tiene funcionalidades declaradas");
+		System.out.println("MODULO PENDIENTE, AUN NO SE CUENTA CON UN EXAMEN ESPECIFICO");
 	}
 }
--- src/exa/ubi/revision/Teens.java
+++ src/exa/ubi/revision/Teens.java
@@ -1,7 +1,61 @@
 package exa.ubi.revision;
 
+import java.util.Scanner;
+
 public class Teens {
+	int uno, dos, tres, cuatro, cinco, seis, siete, ocho, nueve, diez, suma;
+	public Scanner sc = new Scanner(System.in);
+	int lista[] = new int[10];
+
 	public void revisar_teens(){
-		System.out.println("Este metodo TEENS aun no tiene funcionalidades declaradas");
+		System.out.print("¿Cuantas preguntas han respondido bien?\n");
+		int cp=sc.nextInt();
+		
+		if (cp>0 && cp<=10) {
+		for(int i=0;i<cp;i++) {
+			System.out.print("Inserte el numero de la pregunta correcta");
+			System.out.print("\n -> ");
+			lista[i]=sc.nextInt();
+		}
+		
+		for (int i=0;i<cp;i++) {
+			int pregunta=lista[i];
+			
+			if (pregunta==1) {
+				uno=10;
+			} else if(pregunta==2){
+				dos=20;
+			}else if (pregunta==3){
+				tres=5;
+			}
+			else if(pregunta==4) {
+				cuatro=5;
+			} else if (pregunta==5) {
+				cinco=10;
+			} else if (pregunta==6) {
+				seis=20;
+			} else if(pregunta==7) {
+				siete=10;
+			}else if(pregunta==8) {
+				ocho=10;
+			}else if(pregunta==9) {
+				nueve=5;
+			}else if(pregunta==10) {
+				diez=5;
+			}
+			
+		suma=uno+dos+tres+cuatro+cinco+seis+siete+ocho+nueve+diez;
+	}
+	
+	if (suma<=70) {
+		System.out.println("Puntaje obtenido: "+suma+"% "+" SE QUEDA DENTRO DEL NIVEL\n");
+	}
+	else if (suma>70) {
+		System.out.println("Puntaje obtenido: "+suma+"% "+" DEBE PRESENTARSE OTRO NIVEL PARA VERIFICAR NIVEL\n");
+	}
+		}
+		else {
+			System.out.println("EL MAXIMO DE PREGUNTAS ES 10 (Los examenes no contienen más de 10 preguntas)");
+		}
 	}
 }
